abd0a3b52eb909d907dd20ebe3101b98dcefd0e3
Huang <lily.huang@metlife.com>
2020-09-25 17:28:13 +0800

    euraka server

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,14 +20,16 @@ public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
+    @Value("${server.port}")
+    private int port;
 
     @PostMapping("/payment/savePayment")
     public CommenResult savePayment(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         if (result > 0){
-            return new CommenResult(200,"插入成功",result);
+            return new CommenResult(200,"插入成功"+port,result);
         }else{
-            return new CommenResult(-1,"插入失败",null);
+            return new CommenResult(-1,"插入失败"+port,null);
         }
     }
 
@@ -35,10 +38,9 @@ public class PaymentController {
         Payment result = paymentService.getPaymentById(id);
         log.info("插入数据");
         if (Objects.isNull(result)){
-            return new CommenResult(-1,"无ID为{}的数据"+id,null);
+            return new CommenResult(-1,"无ID为{}的数据"+id+"_"+port,null);
         }else {
-            return new CommenResult(200,"查询成功",result);
+            return new CommenResult(200,"查询成功"+port,result);
         }
     }
-
 }
