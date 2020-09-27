6e7f7da1a259ec0d46b2f8a25b8eafb1b125c61b
huangl1 <huang.a.lily@capgemini.com>
2020-09-27 18:44:26 +0800

    ribbon

---
+++ cloud.consumer-ribbonrandom-order80/src/main/java/com/atguigu/springcloud/OrderRibbonMain80.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import com.atguigu.rule.MyRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MyRule.class)
+public class OrderRibbonMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderRibbonMain80.class, args);
+    }
+}
---
+++ cloud.consumer-ribbonrandom-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommenResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@PropertySource("classpath:constant.properties")
+public class OrderController {
+
+    @Value("${RESTTEMPLATE_URL}")
+    private String RESTTEMPLATE_URL;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/payment/create")
+    public CommenResult create(Payment payment){
+        return restTemplate.postForObject(RESTTEMPLATE_URL+"payment/savePayment",payment,CommenResult.class);
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommenResult getPaymentById(@PathVariable("id")Integer id){
+        return restTemplate.getForObject(RESTTEMPLATE_URL+"payment/getPaymentById/"+id,CommenResult.class);
+    }
+
+}
